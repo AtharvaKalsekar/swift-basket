30937870908e5e668184b4321b4049e3ddc1bed5
AtharvaKalsekar <kalsekar.atharva7@gmail.com>
2021-05-15 13:58:30 +0530

    added react-bootdtrap, nav bar and font awsome cdn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import "./bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <h1>ApnaBazaar</h1>
-    </div>
+    <>
+      <Header />
+      <Container>
+        <div className="main py-3">
+          <h1>Swift Basket</h1>
+        </div>
+      </Container>
+      <Footer />
+    </>
   );
 }
 
